e49fe5b2ae00b7844b81b931b157f5104fb6f2dd
Javier Rodrigo <jav.rodrigo.alonso@gmail.com>
2023-01-13 19:44:45 +0100

    readme and general routing

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify
+from markupsafe import escape
 
 app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def upload_file():
+    response = {'message': 'success'}
+    return jsonify(response)
 
 @app.route('/upload_image', methods=['POST'])
 def post_upload():
     response = {'message': 'success'}
     return jsonify(response)
 
-@app.route('/analyse_image/<id>', methods=['GET'])
+@app.route('/analyse_image/<int:id>', methods=['GET'])
 def get_image(id):
     response = {'message': 'success'}
     return jsonify(response)
